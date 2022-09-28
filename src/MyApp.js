0164931c876363d910705380ec6e1a9623c8ab88
Lucas Abdo <luc.abdo01@gmail.com>
2022-09-28 13:45:13 -0700

    added updateList by backend POST

--- src/MyApp.js
+++ src/MyApp.js
@@ -14,9 +14,12 @@ function MyApp() {
      });
  }, [] );
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+ function updateList(person) { 
+  makePostCall(person).then( result => {
+  if (result && result.status === 200)
+     setCharacters([...characters, person] );
+  });
+}
 
 
 function removeOneCharacter (index) {
@@ -38,6 +41,17 @@ function removeOneCharacter (index) {
          return false;         
       }
    }
+
+   async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+ }
 
   return (
     <div className="container">
